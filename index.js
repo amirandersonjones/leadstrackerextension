2c07031d3f7bded1ef9a6d1f34ec0da564c09f31
amirandersonjones <amirandersonjones@gmail.com>
2022-05-10 12:05:18 -0500

    third

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 let myLeads = []
-
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-// localStorage.setItem("myLeads", "www.exampleleads.com" )
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
+
+
 // HINTS:
 // localStorage.setItem(key, value)
 // localStorage.getItem(key)
 // localStorage.clear()
 // PS: both key and value need to be strings
-
-//Get leads from the local storage, store it in a variable, log out the variable
-localStorage.clear()
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
-
-const inputBtn = document.getElementById("input-btn")
-inputBtn.addEventListener("click", function() {
-   myLeads.push(inputEl.value);
-   inputEl.value = ""
-   //save the myLeads array to localStorage
-   localStorage.setItem("myLeads", JSON.stringify(myLeads) ) 
-   renderLeads();
-//    console.log(localStorage.getItem("myLeads"))
-})
-
-
-//log out the items in myLeads array using for loop
-function renderLeads(){
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++){
-    // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
-    listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'> 
-            ${myLeads[i]}
-            </a>
-        </li>
-    `
-        ulEl.innerHTML = listItems
-    }
-}
