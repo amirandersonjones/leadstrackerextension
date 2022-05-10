33d05cbcee281d6cb6c1ef0b17b2a98dc584ba5a
amirandersonjones <amirandersonjones@gmail.com>
2022-05-10 10:32:05 -0500

    second

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-let myLeads = ["www.google.com","www.yahoo.com", "www.amircodes4u.com"]
+let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+// localStorage.setItem("myLeads", "www.exampleleads.com" )
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
 
+//Get leads from the local storage, store it in a variable, log out the variable
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 
 const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
-   myLeads.push(inputEl.value)
-   console.log(inputEl.value)
-   console.log(myLeads)
+   myLeads.push(inputEl.value);
+   inputEl.value = ""
+   //save the myLeads array to localStorage
+   localStorage.setItem("myLeads", JSON.stringify(myLeads) ) 
+   renderLeads();
+//    console.log(localStorage.getItem("myLeads"))
 })
 
+
 //log out the items in myLeads array using for loop
-for (let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> ";
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++){
+    // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
+    listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'> 
+            ${myLeads[i]}
+            </a>
+        </li>
+    `
+        ulEl.innerHTML = listItems
+    }
 }
